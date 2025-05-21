82ba1b264f491f7135a9bed6b27d99f409eafe7a
boualif <farah.bouali.orelservices@gmail.com>
2025-05-21 11:05:48 +0200

    Update settings.py

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,19 +1,15 @@
 from pydantic_settings import BaseSettings # type: ignore
 
 class Settings(BaseSettings):
-    # Make the API key optional with a default empty string
-    ELASTICSEARCH_HOST: str = "localhost"
-    ELASTICSEARCH_PORT: int = 9200
+    # Database settings
+    POSTGRES_URI: str = ""
+    
+    # Elasticsearch settings
+    ELASTICSEARCH_URL: str = ""
     ELASTICSEARCH_INDEX_CANDIDATES: str = "candidates"
     
-    ELASTICSEARCH_URL: str = ""
-    
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        # Construire l'URL complète
-        self.ELASTICSEARCH_URL = f"http://{self.ELASTICSEARCH_HOST}:{self.ELASTICSEARCH_PORT}"
+    # OpenAI settings
     OPENAI_API_KEY: str = ""
-    POSTGRES_URI: str
     
     class Config:
         env_file = ".env"
